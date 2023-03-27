ba075ad7d68924443034f8da1e1b44aec0f8b20e
RPGISAP <rchrist760@gmail.com>
2023-03-27 22:58:14 +0200

    v2.5

--- src/main/java/nl/han/showcase/Birdblitz/entities/Kogel.java
+++ src/main/java/nl/han/showcase/Birdblitz/entities/Kogel.java
@@ -8,6 +8,39 @@ import com.github.hanyaeger.api.entities.SceneBorderCrossingWatcher;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 import com.github.hanyaeger.api.scenes.SceneBorder;
 
+class enemyKogel extends DynamicSpriteEntity implements SceneBorderCrossingWatcher, Collider, Collided {
+	private int schade;
+	private Class schutterClass;
+
+	public enemyKogel(Coordinate2D initialLocation, double angle, int snelheid, int schade, Class schutterClass) {
+		super("entities/enemyprojectile.png", initialLocation);
+
+		setMotion(snelheid, angle);
+		this.schade = schade;
+		this.schutterClass = schutterClass;
+	}
+
+	public enemyKogel(Coordinate2D initialLocation, Speler speler) {
+		super("entities/enemyprojectile.png", initialLocation, new Size(40, 40));
+		setMotion(10, 180);
+	}
+	@Override
+	public void notifyBoundaryCrossing(SceneBorder border) {
+		remove();
+	}
+
+	@Override
+	public void onCollision(Collider collidingObject) {
+		if (collidingObject.getClass() == schutterClass) {
+			Tegenstander tegenstander = (Tegenstander) collidingObject;
+			Tegenstander.ontvangSchade(schade);
+		}
+		remove();
+	}
+
+	public void setImage(String s) {
+	}
+}
 public class Kogel extends DynamicSpriteEntity implements SceneBorderCrossingWatcher, Collider, Collided{
 
 
@@ -22,22 +55,22 @@ public class Kogel extends DynamicSpriteEntity implements SceneBorderCrossingWat
 		this.schutterClass = schutterClass;
 	}
 	public Kogel(Coordinate2D initialLocation) {
-			super("entities/playerprojectile.png", initialLocation, new Size(40, 40));
-			setMotion(10, 180);
-		}
-
-
-		@Override
-		public void notifyBoundaryCrossing(SceneBorder border) {
-			remove();
-		}
-
-		@Override
-		public void onCollision(Collider collidingObject) {
-			if (collidingObject.getClass() == schutterClass) {
-				Tegenstander tegenstander = (Tegenstander) collidingObject;
-				Tegenstander.ontvangSchade(schade);
-			}
-			remove();
-		}
+		super("entities/playerprojectile.png", initialLocation, new Size(40, 40));
+		setMotion(10, 180);
 	}
+
+
+	@Override
+	public void notifyBoundaryCrossing(SceneBorder border) {
+		remove();
+	}
+
+	@Override
+	public void onCollision(Collider collidingObject) {
+		if (collidingObject.getClass() == schutterClass) {
+			Tegenstander tegenstander = (Tegenstander) collidingObject;
+			Tegenstander.ontvangSchade(schade);
+		}
+		remove();
+	}
+}
--- src/main/java/nl/han/showcase/Birdblitz/entities/Scherpschutten2.java
+++ src/main/java/nl/han/showcase/Birdblitz/entities/Scherpschutten2.java
@@ -1,32 +1,34 @@
 package nl.han.showcase.Birdblitz.entities;
-
+import nl.han.showcase.Birdblitz.entities.Kogel.*;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.entities.Collider;
 import javafx.scene.input.KeyCode;
 import nl.han.showcase.Birdblitz.KogelSpawner;
 import nl.han.showcase.Birdblitz.entities.text.ScoreText;
-import
 
 import java.util.Set;
 
 public class Scherpschutten2 extends Tegenstander {
+
     public static int grootte = 40;
-    public static int snelheid = 1;
-    public static int levens = 200;
+    private static int snelheid = 1;
+    private static int levens = 200;
     public static int score = 50;
     public static int schade = 100;
+
     private final KogelSpawner kogelSpawner;
-    public Scherpschutten2(Coordinate2D initialLocation, Speler speler, ScoreText scoreText,KogelSpawner kogelSpawner) {
-        super("entities/Speedy.png", initialLocation, new Size(grootte,grootte), speler, scoreText);
+
+    public Scherpschutten2(Coordinate2D initialLocation, Speler speler, ScoreText scoreText, KogelSpawner kogelSpawner) {
+        super("entities/sluipschutter.png", initialLocation, new Size(grootte, grootte), speler, scoreText);
         this.kogelSpawner = kogelSpawner;
     }
 
     @Override
     public void onCollision(Collider collidingObject) {
-        if(collidingObject instanceof Kogel) {
+        if (collidingObject instanceof Kogel) {
             levens--;
-            if(levens<0) {
+            if (levens < 0) {
                 remove();
                 speler.setScore(speler.getScore() + score);
                 scoreText.setScoreText(speler.getScore());
@@ -34,21 +36,20 @@ public class Scherpschutten2 extends Tegenstander {
             }
         }
     }
+
     @Override
     public void Beweeg() {
-        setMotion(snelheid,360);
+        setMotion(snelheid, 360);
     }
-    @Override
+
     @Override
     public void onPressedKeysChange(Set<KeyCode> pressedKeys) {
         if (pressedKeys.contains(KeyCode.SPACE)) {
-            // Spawn a bullet at the current location of the Scherpschutten2 entity
-            Coordinate2D bulletLocation = getLocation();
-            Kogel kogel = new Kogel(bulletLocation, speler);
-            getSceneManager().addEntity(kogel);
+
+            // Spawn a bullet at the current location of the Scherpschutten2 entity that travels downwards
+            Coordinate2D bulletLocation = (Coordinate2D) getLocationInScene().add(getWidth() / 2, getHeight() / 2);
+            enemyKogel kogel = new enemyKogel(bulletLocation, 90, 10, schade, getClass());
+            kogel.setImage("entities/enemyprojectile.png");
         }
     }
-
-
 }
-
